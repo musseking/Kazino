aa6a4d587e66bdf261719fee95b2f975557f55dc
musseking <81393756+musseking@users.noreply.github.com>
2023-10-16 23:14:29 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,43 +4,44 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+	        Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Koliko kladionicara igra? ");
-        int size = scanner.nextInt();
-        Kladionicar[] nizKladionicara = new Kladionicar[size];
+        	int size = scanner.nextInt();
+        	Kladionicar[] nizKladionicara = new Kladionicar[size];
 
 		String name;
 		String game;
-        int number;
+        	int number;
 
-        for(int i = 0; i < nizKladionicara.length; ++i) {
-            System.out.print("Unesite ime kladionicara broj " + (i + 1) + ". ");
-            name = scanner.next();
+        	for(int i = 0; i < nizKladionicara.length; ++i) {
+            	    System.out.print("Unesite ime kladionicara broj " + (i + 1) + ". ");
+            	    name = scanner.next();
 
-			System.out.print("Unesite igru (malim slovima) koju igra kladionicar broj " + (i + 1) + ". ");
-			game = scanner.next();
+	    	    System.out.print("Unesite igru (malim slovima) koju igra kladionicar broj " + (i + 1) + ". ");
+	            game = scanner.next();
 
-			System.out.print("Unesite koliko para ima kladionicar broj " + (i + 1) + ". ");
-			number = scanner.nextInt();
+	            System.out.print("Unesite koliko para ima kladionicar broj " + (i + 1) + ". ");
+	            number = scanner.nextInt();
 
-            Kladionicar noviKladionicar = new Kladionicar(name, game, number);
-            nizKladionicara[i] = noviKladionicar;
-        }
+                    Kladionicar noviKladionicar = new Kladionicar(name, game, number);
+                    nizKladionicara[i] = noviKladionicar;
+		}
 
 		System.out.println();
-        System.out.print("Niz napravljenih kladionicara je: [");
+        	System.out.print("Niz napravljenih kladionicara je: [");
 
-        for(int i = 0; i < nizKladionicara.length; ++i) {
-           System.out.print(nizKladionicara[i] + ", ");
-           Kladionicar lastObject = nizKladionicara[size - 1];
-           if (nizKladionicara[i].equals(lastObject)) {
-               System.out.print("\b" + "\b");
-               System.out.print("]");
-           }
-        }
+        	for(int i = 0; i < nizKladionicara.length; ++i) {
+           	    System.out.print(nizKladionicara[i] + ", ");
+                    Kladionicar lastObject = nizKladionicara[size - 1];
+			
+           	    if (nizKladionicara[i].equals(lastObject)) {
+               		System.out.print("\b" + "\b");
+               		System.out.print("]");
+           	    }
+        	}
 
-        System.out.println();
+        	System.out.println();
 
 		Kazino kazino = new Kazino(nizKladionicara);
         
